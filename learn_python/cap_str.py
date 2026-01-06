829cb92d98b4b0582d79f058f0c0d3a886cc0874
Ana Budai <anamaria_b2006@yahoo.com>
2026-01-06 22:06:07 +0200

    Practice Python: strings

--- learn_python/cap_str.py
+++ learn_python/cap_str.py
@@ -1,1 +1,14 @@
 
+template = "My name is {} and I have {} years old"
+
+name = "Ana"
+age = 33
+
+print(template.format(name, age))
+
+print(f"My name is {name} and I have {age} years old")
+
+print()
+
+print(chr(65))
+print(ord("A"))
